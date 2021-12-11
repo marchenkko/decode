d03d93fb86d9ecc5f8ee935406ea1820b900bd03
marchenkko <marchenkovlada1802@gmail.com>
2021-12-11 17:40:30 +0300

    fix

--- add_users.php
+++ add_users.php
@@ -11,4 +11,18 @@ $stmt->bindValue(':sex', $sex);
 
 
 $stmt->execute();
+
+$userid=$connection->lastInsertId();
+$serial=rand(1,100000);
+$number=rand(1,10000);
+$date_int= rand(1262055681,1262055681);
+$date = date("Y-m-d H:i:s",$date_int);
+$stmt = $connection->prepare("INSERT INTO passport (user_id, serial,number,date) 
+VALUES (:user_id,:serial,:number,:date )");
+$stmt->bindValue(':user_id', $userid);
+$stmt->bindValue(':serial',$serial);
+$stmt->bindValue(':number',$number);
+$stmt->bindValue(':date',$date);
+$stmt->execute();
+
 echo "New records created successfully";
